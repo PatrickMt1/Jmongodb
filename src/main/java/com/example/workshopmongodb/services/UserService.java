5bac4c8d16af5bc2ed8070ffa93110cd4e8d7107
Patrick Mota <patrickmota.js@hotmail.com>
2025-05-21 21:42:10 -0300

    Buscando usuário por ID e Inserindo com o Metodo POST

--- src/main/java/com/example/workshopmongodb/services/UserService.java
+++ src/main/java/com/example/workshopmongodb/services/UserService.java
@@ -1,12 +1,15 @@
 package com.example.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.workshopmongodb.domain.User;
+import com.example.workshopmongodb.dto.UserDTO;
 import com.example.workshopmongodb.repository.UserRepository;
+import com.example.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +21,16 @@ public class UserService {
         return repo.findAll();
     }
 
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
 }
