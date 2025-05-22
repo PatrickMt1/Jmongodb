9ddddbf2d3e2f9ccf82851af857720b82d0cb8f4
Patrick Mota <patrickmota.js@hotmail.com>
2025-05-22 20:53:45 -0300

    Instanciando o Método DELETE e o Método PUT

--- src/main/java/com/example/workshopmongodb/services/UserService.java
+++ src/main/java/com/example/workshopmongodb/services/UserService.java
@@ -30,7 +30,23 @@ public class UserService {
         return repo.insert(obj);
     }
 
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    public void delete(String id) {
+        findById(id);
+        repo.deleteById(id);
+    }
+
     public User fromDTO(UserDTO objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
     }
 }
