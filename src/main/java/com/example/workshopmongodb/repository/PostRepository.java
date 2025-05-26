8097194e1dca684e8d2b78bd0f9049815426d56b
Patrick Mota <patrickmota.js@hotmail.com>
2025-05-26 16:37:17 -0300

    Consulta com vários critérios

--- src/main/java/com/example/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/example/workshopmongodb/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.example.workshopmongodb.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -11,11 +12,14 @@ import com.example.workshopmongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
-    @Query("{ 'title': { $regex: ?0 $options: 'i' } }")
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
     List<Post> searchTitle(String text);
 
     //é um método de consulta derivada no Spring Data JPA ou Spring Data MongoDB.
     //É quando o nome do método já indica automaticamente ao Spring qual consulta deve ser executada, sem precisar escrever uma query manualmente.
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ $and: [ {date: {$gte: ?1} }, {date: {$lte: ?2 } },{ $or: [ {'title': { $regex: ?0 ,$options: 'i'}}, {'body': { $regex: ?0 ,$options: 'i'}},{'comments.text': { $regex: ?0 ,$options: 'i'}} ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
