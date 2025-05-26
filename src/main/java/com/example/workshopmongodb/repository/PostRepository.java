14b5f7cdb7586063e56e19bea9d2a35bc8a3367a
Patrick Mota <patrickmota.js@hotmail.com>
2025-05-26 15:57:40 -0300

    Consulta simples com Query Methods e com @Query

--- src/main/java/com/example/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/example/workshopmongodb/repository/PostRepository.java
@@ -1,11 +1,21 @@
 package com.example.workshopmongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.workshopmongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0 $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
+    //é um método de consulta derivada no Spring Data JPA ou Spring Data MongoDB.
+    //É quando o nome do método já indica automaticamente ao Spring qual consulta deve ser executada, sem precisar escrever uma query manualmente.
+    List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
